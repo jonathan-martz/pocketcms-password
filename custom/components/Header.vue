2308fba0f9fecd31a067265f72d5904433a49075
jonathan-martz <github@jmartz.de>
2026-01-01 15:37:15 +0100

    add bars

--- custom/components/Header.vue
+++ custom/components/Header.vue
@@ -1,5 +1,21 @@
 <template>
-  <section class="bg-white px-3 py-3">
+  <section class="bg-white px-3 py-3 flex justify-between">
     <a href="/" class="text-primary">PocketPass.io</a>
+    <nav>
+      <section v-if="open" class="bars">
+        <button @click="open = !open">
+          <font-awesome-icon :icon="['fas', 'bars']" size="2x"/>
+        </button>
+      </section>
+      <section v-else class="times">
+        <button @click="open = !open">
+          <font-awesome-icon :icon="['fas', 'times']" size="2x"/>
+        </button>
+      </section>
+    </nav>
   </section>
 </template>
+
+<script setup lang="ts">
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+</script>
