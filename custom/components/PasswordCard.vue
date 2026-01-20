f785b09ecc3d2669949d1bb64977fc153545e06b
jonathan-martz <github@jmartz.de>
2026-01-20 22:37:55 +0100

    add show button for password, move delete button

--- custom/components/PasswordCard.vue
+++ custom/components/PasswordCard.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="card bg-gray-400 shadow-sm">
     <div class="card-body">
-      <h2 class="card-title">{{ props.data.name }}</h2>
-      <p v-if="props.data.notes" class="break-all">{{props.data.notes}}</p>
-      <p v-if="props.data.username">{{props.data.username}}</p>
-      <div class="card-actions justify-end">
-        <button @click="remove(props.data.id)" class="btn btn-error btn-sm">
-          <font-awesome-icon :icon="['fas','trash']"></font-awesome-icon>
-        </button>
-        <button @click="copyText(props.data.username)" v-if="isHttps" class="btn btn-primary btn-sm">
-          <font-awesome-icon :icon="['fas','copy']"></font-awesome-icon>
-          <span>Username</span>
-        </button>
-        <button @click="copyPassword()" v-if="isHttps" class="btn btn-primary btn-sm">
-          <font-awesome-icon :icon="['fas','copy']"></font-awesome-icon>
-          <span>Password</span>
-        </button>
-        <p v-if="!isHttps" class="alert alert-error">Verbindung ist nicht sicher</p>
+      <div class="flex justify-between">
+        <h2 class="card-title">{{ props.data.name }}</h2>
+        <section class="actions">
+          <button @click="remove(props.data.id)" class="btn btn-error btn-sm">
+            <font-awesome-icon :icon="['fas','trash']"></font-awesome-icon>
+          </button>
+        </section>
+      </div>
+      <div v-if="isHttps">
+        <p v-if="props.data.notes" class="break-all">{{ props.data.notes }}</p>
+        <p v-if="props.data.username">{{ props.data.username }}</p>
+        <div class="card-actions justify-end mt-3">
+          <button @click="showPassword()" class="show btn btn-primary btn-sm">
+            <font-awesome-icon :icon="['fas','eye']"></font-awesome-icon>
+            <span>{{show}}</span>
+          </button>
+          <button @click="copyText(props.data.username)" v-if="isHttps" class="btn btn-primary btn-sm">
+            <font-awesome-icon :icon="['fas','copy']"></font-awesome-icon>
+            <span>Username</span>
+          </button>
+          <button @click="copyPassword()" v-if="isHttps" class="btn btn-primary btn-sm">
+            <font-awesome-icon :icon="['fas','copy']"></font-awesome-icon>
+            <span>Password</span>
+          </button>
+        </div>
+      </div>
+      <div v-else>
+        <section class="alert alert-error">
+          unsecure Website
+        </section>
       </div>
     </div>
   </div>
@@ -28,6 +42,7 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 const privKey = useLocalStorage('privKey', '');
 const pb = usePocketBase()
+const show = ref('???');
 
 const props = defineProps({
   data: {
@@ -41,13 +56,25 @@ const copyPassword = async () => {
   );
 };
 
-const isHttps = location.protocol === 'https:'
+const isHttps = location.protocol === 'https:' || location?.hostname === 'localhost'
 
 const remove = async (id) => {
   if (confirm('Willst du es wirklich löschen ?')) {
     await pb.collection('passwords').delete(id)
   }
 }
+
+const showPassword = async () => {
+  console.log('encypretd', props.data.encrypted);
+  console.log('privkey', privKey.value);
+if(show.value === '???'){
+  show.value = await decryptMessage(props.data.encrypted, privKey.value, 'password')
+}
+else{
+  show.value = '???'
+}
+
+};
 
 function copyText(text) {
   navigator.clipboard.writeText(text);
